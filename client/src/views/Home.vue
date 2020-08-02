d5e6f872804744cbaefa53f05dadb43b98122d7b
Andrej Peternel <peternelandrej0@gmail.com>
2020-08-02 20:35:48 +0200

    Vue app initial commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div>
+      <SidebarButton/>
+    </div>
+    <img id="logo" alt="Vue logo" src="../assets/RadioV10Logo.png">
+    <Main/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'Home',
+  components:{
+    Main
+  }
+}
+</script>
