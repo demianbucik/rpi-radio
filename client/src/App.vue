bf0bde9b3e4a7caa30633a4ee8982c7e4ca085fe
andpet27 <62115025+andpet27@users.noreply.github.com>
2020-08-02 20:40:40 +0200

    Vue app initial commit (#1)

---
+++ client/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <SidebarButton id="sidebarButton"/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import SidebarButton from '@/components/SidebarButton.vue'
+
+export default {
+  name: 'App',
+  components:{
+    SidebarButton
+  }
+}
+</script>>
+
+<style>
+html{
+  height: 100%;
+}
+
+body{
+  background-image: linear-gradient(to bottom right, #696969 70%, #4b4b4b 100%);
+}
+
+#sidebarButton{
+  position: absolute;
+  top: 30px;
+  left: 30px;
+}
+
+#logo{
+  max-width: 60%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #b0cbe6;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ client/src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Welcome to Radio Vipavska 10</h1>
+        <h3>NO MEME MUSIC</h3>
+    </div>
+</template>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>>
