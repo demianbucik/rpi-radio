ead188f2c56d4c3c598c361cb69594dd68e35872
demianbucik <demian.bucik@gmail.com>
2020-10-19 00:21:46 +0200

    Endpoints

--- radio/api/views.go
+++ radio/api/views.go
@@ -2,24 +2,37 @@ package api
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"gorm.io/gorm"
 
 	"radio/api/playlists"
 	"radio/api/tracks"
+	"radio/api/utils"
 )
 
-func Init(db *gorm.DB, router *chi.Mux) {
+func NewRouter(db *gorm.DB) *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(utils.RequestCtx)
+	router.Use(utils.Logger)
+	router.Use(utils.Recoverer)
+	router.Use(middleware.Heartbeat("/ping"))
 
 	playlistsApi := playlists.New(db)
 	tracksApi := tracks.New(db)
 
+	playlistRoutes(router, playlistsApi)
+	tracksRoutes(router, tracksApi)
+
+	return router
+}
+
+func playlistRoutes(router *chi.Mux, playlistsApi *playlists.Api) {
 	router.Route("/playlists", func(r chi.Router) {
 		r.Get("/", playlistsApi.List)
 		r.Post("/", playlistsApi.Create)
 
 		r.Route("/{playlistId:[0-9]+}", func(r chi.Router) {
 			r.Use(playlistsApi.PlaylistCtx)
-
 			r.Get("/", playlistsApi.Get)
 			r.Put("/", playlistsApi.Update)
 			r.Delete("/", playlistsApi.Delete)
@@ -30,21 +43,20 @@ func Init(db *gorm.DB, router *chi.Mux) {
 
 				r.Route("/{position:[0-9]+}", func(r chi.Router) {
 					r.Use(playlistsApi.PlaylistTrackCtx)
-
 					r.Delete("/", playlistsApi.DeleteTrack)
 				})
 			})
 		})
-
 	})
+}
 
+func tracksRoutes(router *chi.Mux, tracksApi *tracks.Api) {
 	router.Route("/tracks", func(r chi.Router) {
 		r.Get("/", tracksApi.List)
 		r.Post("/", tracksApi.Create)
 
 		r.Route("/{trackId:[0-9]+}", func(r chi.Router) {
 			r.Use(tracksApi.TrackCtx)
-
 			r.Put("/", tracksApi.Update)
 			r.Delete("/", tracksApi.Delete)
 		})
