0710f405726b71367d8c832201ac19df427bd33f
demianbucik <demian.bucik@gmail.com>
2020-10-19 00:24:31 +0200

    Env file

---
+++ radio/app/app.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"os"
+	"radio/app/config"
+)
+
+const (
+	EnvProd = "prod"
+)
+
+var Dev bool
+
+func Startup() {
+	parseDevFlag()
+	configureLogger()
+	config.Parse()
+}
+
+func parseDevFlag() {
+	Dev = os.Getenv("RADIO_ENV") != EnvProd
+}
